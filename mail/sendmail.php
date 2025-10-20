3f62fdeba0a19ebab2127ba481cb4f556df15317
quangkhoa11 <khoa110303@gmail.com>
2025-10-20 08:35:16 +0700

    Fix file sendmail.php

--- mail/sendmail.php
+++ mail/sendmail.php
@@ -10,24 +10,21 @@ function sendMail($to, $subject, $body) {
     $mail = new PHPMailer(true);
 
     try {
-        // --- Cấu hình Gmail SMTP ---
         $mail->isSMTP();
         $mail->Host       = 'smtp.gmail.com';
         $mail->SMTPAuth   = true;
-        $mail->Username   = 'khoa110303@gmail.com'; // Gmail của bạn
-        $mail->Password   = 'moim dwoq jhot dptx'; // Mật khẩu ứng dụng
+        $mail->Username   = 'khoa110303@gmail.com'; 
+        $mail->Password   = 'moim dwoq jhot dptx'; 
         $mail->SMTPSecure = 'tls';
         $mail->Port       = 587;
-        $mail->CharSet    = 'UTF-8'; // ⚠️ Thêm dòng này để gửi tiếng Việt chuẩn
+        $mail->CharSet    = 'UTF-8'; 
 
-        // --- Thông tin người gửi / nhận ---
         $mail->setFrom('khoa110303@gmail.com', 'CafeShop System');
         $mail->addAddress($to);
 
-        // --- Nội dung ---
         $mail->isHTML(true);
-        $mail->Subject = $subject;  // ví dụ: "Mã xác nhận đăng ký tài khoản CafeShop"
-        $mail->Body    = $body;     // ví dụ: "Xin chào, mã xác nhận của bạn là 123456"
+        $mail->Subject = $subject;  
+        $mail->Body    = $body;    
 
         $mail->send();
         return true;
