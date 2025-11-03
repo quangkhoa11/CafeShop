01567f83cec00474ad06334824a2e9f9e871b90d
quangkhoa11 <khoa110303@gmail.com>
2025-11-03 16:34:58 +0700

    push

---
+++ page/return/index.php
@@ -0,0 +1,10 @@
+<?php
+date_default_timezone_set('Asia/Ho_Chi_Minh');
+
+if (!empty($_GET)) {
+    file_put_contents(__DIR__ . "/return_log.txt", date("Y-m-d H:i:s") . " | " . json_encode($_GET) . PHP_EOL, FILE_APPEND);
+}
+
+header("Location: index.php?page=menu");
+exit();
+?>
