12f2b870be7cf4748c5637f65ef7eb303ecc02e9
quangkhoa11 <khoa110303@gmail.com>
2025-11-05 10:58:32 +0700

    remove

--- page/cart/remove_item.php
+++ page/cart/remove_item.php
@@ -1,8 +1,16 @@
 <?php
 session_start();
-if (isset($_POST['idsp'])) {
-    $idsp = $_POST['idsp'];
-    unset($_SESSION['cart'][$idsp]);
-    echo "ok";
+
+if (isset($_POST['cart_key'])) {
+    $cart_key = $_POST['cart_key'];
+
+    if (isset($_SESSION['cart'][$cart_key])) {
+        unset($_SESSION['cart'][$cart_key]);
+        echo "ok";
+    } else {
+        echo "not_found";
+    }
+} else {
+    echo "no_key";
 }
 ?>
